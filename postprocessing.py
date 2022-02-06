27fcabbedd3d34e94b95cd127dee64c0e5034da7
s25jin <32162576+s25jin@users.noreply.github.com>
2022-02-06 11:29:27 +0900

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -4,7 +4,7 @@ from feedgen.feed import FeedGenerator
 from datetime import datetime, timezone
 
 # Normal GitHub Pages URL
-# ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
+# ghPagesURL = "https://github.com/s25jin/tiktok-rss-flat/"
 
 # Custom Domain
 ghPagesURL = "https://tiktokrss.conoroneill.com/"
